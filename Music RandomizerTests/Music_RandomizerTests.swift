c2fdb85aaaa02e04037052ee6fd6153537289dba
Andy Keller <pulazzo@gmail.com>
2014-08-07 14:31:24 -0400

    Added tests for SimpleList

--- Music RandomizerTests/Music_RandomizerTests.swift
+++ Music RandomizerTests/Music_RandomizerTests.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 import XCTest
+import Music_Randomizer
 
 class Music_RandomizerTests: XCTestCase {
     
@@ -32,5 +33,47 @@ class Music_RandomizerTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    
+    func testEmptyList() {
+        let list = SimpleList()
+        XCTAssertEqual(list.count, 0, "List should have 0 entries")
+        XCTAssertNil(list.head, "We should not have a head")
+        XCTAssertNil(list.tail, "We should not have a tail")
+    }
+    
+    func testAppend() {
+        let list = SimpleList()
+        list.append("1")
+        XCTAssertEqual(list.count, 1, "List should have 1 entry")
+        XCTAssertNotNil(list.head, "We should have a head")
+        XCTAssertNotNil(list.tail, "We should have a tail")
+    }
+    
+    func testIteration() {
+        let list = SimpleList()
+        list.append("1")
+        list.append("2")
+        XCTAssertEqual(list.count, 2, "List should have 2 entries")
+        
+        XCTAssertEqual("1", list.head!.value as String, "Head");
+        XCTAssertEqual("2", list.tail!.value as String, "Tail");
+        
+        let iter = list.iterator()
+        
+        XCTAssertTrue(iter.hasNext(), "Should have next")
+        let (oneValue: AnyObject, oneIndex) = iter.next()!
+        XCTAssertEqual("1", oneValue as String, "One")
+        XCTAssertEqual(0, oneIndex, "Index")
+        
+        XCTAssertTrue(iter.hasNext(), "Should have next")
+        let (twoValue: AnyObject, twoIndex) = iter.next()!
+        XCTAssertEqual("2", twoValue as String, "Two")
+        XCTAssertEqual(1, twoIndex, "Index")
+        
+        XCTAssertFalse(iter.hasNext(), "Should not have next")
+    }
+    
+
     
 }
